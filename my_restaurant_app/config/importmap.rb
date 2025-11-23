e72eea3358b0297f3e7e75fd594da37cdc9d5458
aki-droide <aki_m_415@yahoo.co.jp>
2025-11-23 23:23:24 +0900

    005issue達成

---
+++ my_restaurant_app/config/importmap.rb
@@ -0,0 +1,15 @@
+# config/importmap.rb
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Bootstrap関連の追加
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+
+# jQuery（Bootstrap用）
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
