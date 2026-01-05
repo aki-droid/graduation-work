0903020e4287828d15c8dbaf11da7b528ce796c9
aki-droide <aki_m_415@yahoo.co.jp>
2026-01-05 23:52:56 +0900

    Make add_user reference migration idempotent

--- my_restaurant_app/db/migrate/20251214034823_add_user_id_to_restaurants.rb
+++ my_restaurant_app/db/migrate/20251214034823_add_user_id_to_restaurants.rb
@@ -1,5 +1,7 @@
 class AddUserIdToRestaurants < ActiveRecord::Migration[7.0]
   def change
-    add_reference :restaurants, :user, foreign_key: true
+    unless column_exists?(:restaurants, :user_id)
+      add_reference :restaurants, :user, foreign_key: true
+    end
   end
 end
